0ca4c90da078fc56119bd380754c3c83fe1f19f8
ylvaj <129056937+ylvaj@users.noreply.github.com>
2023-07-30 09:54:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@
 
 sudo apt-get purge ffmpeg -y > /dev/null 2>&1
 curl -sSL https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o /tmp/ffmpeg-master-latest-linux64-gpl.tar.xz
-tar xf /tmp/ffmpeg-master-latest-linux64-gpl.tar.xz
-sudo mv ~/ffmpeg-master-latest-linux64-gpl/bin/* /usr/local/bin
+tar xf /tmp/ffmpeg-master-latest-linux64-gpl.tar.xz -C /tmp
+sudo mv /tmp/ffmpeg-master-latest-linux64-gpl/bin/* /usr/local/bin
 sudo chmod a+rx /usr/local/bin/ffmpeg /usr/local/bin/ffplay /usr/local/bin/ffprobe
 sudo rm -fr ~/ffmpeg-master-latest-linux64-gpl
 
 sudo curl -sSL https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo curl -sSL https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/yt-dlp /usr/local/bin/youtube-dl
-echo "force-unsafe-io" | sudo tee /etc/dpkg/dpkg.cfg.d/02apt-speedup
+#echo "force-unsafe-io" | sudo tee /etc/dpkg/dpkg.cfg.d/02apt-speedup
 DEBIAN_FRONTEND="noninteractive" TZ="Europe/Berlin" sudo apt-get update > /dev/null 2>&1
 DEBIAN_FRONTEND="noninteractive" TZ="Europe/Berlin" sudo apt-get install -y rename \
     gawk rsync python3-pip git ncdu rename mkvtoolnix vim aria2 python3-pip \
